3f1b1f7c52f7cccfdbd103e5289fb02c5d036d3d
Sergei Finogenov <serfinogenov@yandex.ru>
2025-04-29 00:16:52 +0300

    creating class 'DBManager' and its methods to deal with selected employers and their vacancies; creating several functions in the module 'utils.py' helping to realize the code; creating 'main' function

---
+++ main.py
@@ -0,0 +1,23 @@
+from config import config
+from src.dbmanager import DBManager
+from src.utils import create_database, fill_in_employers, create_tables, get_employers_from_hh, get_vacancies_from_hh, \
+    fill_in_vacancies, get_keyword, user_interaction
+
+
+def main() ->:
+    """Основная функция, объединяющая функционал"""
+    params = config()
+    employers_to_fill_in_tables = get_employers_from_hh()
+    vacancies_to_fill_in_tables = get_vacancies_from_hh()
+
+    create_database("hhdatabase", params)
+    create_tables("hhdatabase", params)
+
+    fill_in_employers(employers_to_fill_in_tables,"hhdatabase", params)
+    fill_in_vacancies(vacancies_to_fill_in_tables,"hhdatabase", params)
+
+    user_interaction("hhdatabase", params)
+
+
+if __name__ == '__main__':
+    main()
--- src/hh_api.py
+++ src/hh_api.py
@@ -15,6 +15,7 @@ class HeadHunter(BaseApi):
         self.__url = "https://api.hh.ru"
         self.__params = {"page": 0, "per_page": 100}
         self.__employers = []
+        self.__vacancies = []
 
     def _BaseApi__connect_api(self) -> None:
         """Метод для проверки подключения к API"""
@@ -38,36 +39,11 @@ class HeadHunter(BaseApi):
         else:
             return self.__employers
 
-    def get_vacancies(self, employer_id: int) -> list:
+    def get_vacancies(self, selected_employers: list) -> list:
         """Метод получения данных о вакансиях работодателя с сайта hh.ru"""
-        vacancies = []
         self._BaseApi__connect_api()
-        response = requests.get(f"{self.__url}/vacancies?employer_id={employer_id}")
-        selected_vacancies = response.json()["items"]
-        vacancies.extend(selected_vacancies)
-        return vacancies
-
-
-# Пример использования:
-# ids = [
-#     9694561,
-#     3529,
-#     53797,
-#     227780,
-#     3809,
-#     78638,
-#     907345,
-#     84585,
-#     2180,
-#     3127,
-# ]
-# hh_obj = HeadHunter()
-# found_employers = hh_obj.get_employers(ids)
-#
-# for employer in found_employers:
-#     print(employer['id'], employer['name'])
-#
-#     vacancies_of_employer = hh_obj.get_vacancies(employer["id"])
-#
-#     for vacancy in vacancies_of_employer:
-#         print(vacancy['id'], vacancy['name'], vacancy['alternate_url'], vacancy['salary'], vacancy['employer'])
+        for employer in selected_employers:
+            response = requests.get(f"{self.__url}/vacancies?employer_id={employer['id']}")
+            selected_vacancies = response.json()["items"]
+            self.__vacancies.extend(selected_vacancies)
+        return self.__vacancies
